151f5a68c908c8670f13477256dbaef959a4da44
unknown <shh9904@gmail.com>
2019-08-21 21:55:08 +0900

    enum++

--- Assets/Scripts/Values.cs
+++ Assets/Scripts/Values.cs
@@ -6,6 +6,21 @@
     };
 
     public enum MazeWays {
-        // TODO
+        right = 0,
+        up = 1,
+        left = 2,
+        down = 3,
+        rightup = 4,
+        upleft = 5,
+        leftdown = 6,
+        downright = 7,
+        rightleft = 8,
+        updown = 9,
+        rightupleft = 10,
+        upleftdown = 11,
+        leftdownright = 12,
+        downrightup = 13,
+        all = 14,
+        none = 15
     };
 }
